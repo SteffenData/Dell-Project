0613906a3513f344b2145878cd3f2b34a6a0eb85
juhlmadsen1982@gmail.com <juhlmadsen1982@gmail.com>
2015-04-08 15:55:27 +0200

    Create med file virker endnu ikke. arbejder videre i morgen

--- group_02_2sem/test/dk/group_02/TestSprintOne/TestofCreateProject.java
+++ group_02_2sem/test/dk/group_02/TestSprintOne/TestofCreateProject.java
@@ -10,6 +10,9 @@ import dk.group_02.Entity.Project;
 import dk.group_02.data.DataManager;
 import dk.group_02.Utility.DataValidator;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,36 +26,39 @@ import static org.junit.Assert.*;
  * @author steffen
  */
 public class TestofCreateProject {
-
+    
     Partner partner;
     Project project;
-
+    
     @Before
     public void setUp() {
-      
-        partner = new Partner ("Dell","Denmark");
-        project = new Project ("1992-10-10","huli",1.0,"gold","silver","mikkel");
+        
+        partner = new Partner("Dell", "Denmark");
+        project = new Project("1992-10-10", "huli", 1.0, "gold", "silver", "mikkel");
+        
     }
-
+    
     @Test
-    public void testOpretTrue() throws ClassNotFoundException, SQLException {
+    public void testOpretTrue() throws ClassNotFoundException, SQLException, NullPointerException, FileNotFoundException {
         
         DataManager.SaveProject(project, partner);
-        assertTrue(project.getProjectName().equals( DataManager.getProject(project, partner).getProjectName()));
-       
+        assertTrue(project.getProjectName().equals(DataManager.getProject(project, partner).getProjectName()));
         
     }
-    @Test
-    public void testOpretMedFileTrue(){
-   
-     
-    }
 
+    @Test
+    public void testOpretMedFileTrue() throws ClassNotFoundException, SQLException, FileNotFoundException {
+        File file = new File("C:\\Users\\steffen\\Documents\\NetBeansProjects\\eksamensProjectDell\\group_02_2sem\\task liste.jpg");
+        project.setUpload(file);
+        DataManager.SaveProject(project, partner);
+        
+        assertTrue(project.getUpload().equals(DataManager.getUpload(project, partner)));
+        
+    }
+    
     @Test
     public void testOpretFalse() throws ClassNotFoundException, SQLException {
-       
+        
     }
     
-    
-
 }
