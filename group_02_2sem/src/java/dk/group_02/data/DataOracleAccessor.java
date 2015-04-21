93c59510399c92b642066eeb4e59eb8a037f0817
juhlmadsen1982@gmail.com <juhlmadsen1982@gmail.com>
2015-04-21 14:53:31 +0200

    derby database script added

--- group_02_2sem/src/java/dk/group_02/data/DataOracleAccessor.java
+++ group_02_2sem/src/java/dk/group_02/data/DataOracleAccessor.java
@@ -14,7 +14,7 @@ package dk.group_02.data;
 
         public static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
         public static final String DB_URL = "jdbc:oracle:thin:@datdb.cphbusiness.dk:1521:DAT";
-        public static final String USERNAME = "cphsm";
-        public static final String PASSWORD = "cphsm";
+        public static final String USERNAME = "cphsm182";
+        public static final String PASSWORD = "cphsm182";
 
     }
